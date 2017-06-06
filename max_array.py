b9a3752823fe3337d44968a554925a5a069b5ef2
Pablo Estrela <pablo.estrela@ccc.ufcg.edu.br>
2017-06-05 23:11:02 -0300

    minimo

--- max_array.py
+++ max_array.py
@@ -5,6 +5,13 @@ def max_array(array):
             max = e
     return max
 
+def min_array(array):
+    min = 2000000
+    for e in array:
+        if e < min:
+            min = e
+    return min
 if __name__ == "__main__":
     array = [2,3,4,5,6,7,78,321,2,12,1,2]
     print(max_array(array)) 
+    print(min_array(array)) 
